6deccd3a1ce6b204aeba1163d7f452d13a10da90
Ravi Ranjan <yourfriendraviranjan@gmail.com>
2020-10-22 04:55:07 +0530

    Added Welcome JFrame

--- src/hotel/management/system/HotelManagementSystem.java
+++ src/hotel/management/system/HotelManagementSystem.java
@@ -1,14 +1,46 @@
 package hotel.management.system;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class HotelManagementSystem extends JFrame {
 
     HotelManagementSystem(){
         setBounds(0, 100, 1366, 565);
 
+        ImageIcon image1 = new ImageIcon(ClassLoader.getSystemResource("hotel/management/system/icons/first.jpg"));
+        JLabel label1 = new JLabel(image1);
+        label1.setBounds(0,0,1366, 565);
+        add(label1);
+
+        JLabel label2 = new JLabel("Hotel Management System");
+        label2.setBounds(780, 60, 1000, 70);
+        label2.setForeground(Color.WHITE);
+        label2.setFont(new Font("Sans", Font.PLAIN, 40));
+
+        label1.add(label2);
+
+        JButton button1 = new JButton("Enter");
+        button1.setBackground(Color.DARK_GRAY);
+        button1.setForeground(Color.WHITE);
+        button1.setBorder(null);
+        button1.setFont(new Font("Sans", Font.PLAIN, 20));
+        button1.setBounds(930, 450, 150, 40);
+        label1.add(button1);
+
         setLayout(null);
         setVisible(true);
+
+        while(true){
+            label2.setVisible(false);
+            try{
+                Thread.sleep(500);
+            }catch (Exception e){}
+            label2.setVisible(true);
+            try{
+                Thread.sleep(500);
+            }catch (Exception e){}
+        }
 
 
     }
