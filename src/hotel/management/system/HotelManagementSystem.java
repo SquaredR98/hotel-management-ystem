ce6f170f8ba87e4ab2a993f61239e560f3f44ac9
Ravi Ranjan <yourfriendraviranjan@gmail.com>
2020-10-22 04:29:25 +0530

    Initial commit

---
+++ src/hotel/management/system/HotelManagementSystem.java
@@ -0,0 +1,20 @@
+package hotel.management.system;
+
+import javax.swing.*;
+
+public class HotelManagementSystem extends JFrame {
+
+    HotelManagementSystem(){
+        setBounds(0, 100, 1366, 565);
+
+        setLayout(null);
+        setVisible(true);
+
+
+    }
+
+    public static void main(String[] args){
+        new HotelManagementSystem();
+    }
+
+}
